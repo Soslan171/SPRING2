e80525290fc00d2d07caec518b22d1e32346e6bf
Soslan171 <139155318+Soslan171@users.noreply.github.com>
2024-08-06 12:06:17 +0300

    Add files via upload

---
+++ Task-spr2/src/main/java/app/config/AppConfig.java
@@ -0,0 +1,19 @@
+package app.config;
+
+import app.model.AnimalsCage;
+import app.model.Dog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Timer;
+
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+
+
+
+
+}
